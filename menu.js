df8145d429a8e40be5dfc66f2e257cde0abe3ce9
Xhibalba <47706299+Seventty@users.noreply.github.com>
2020-03-22 06:57:46 -0400

    Js update

--- menu.js
+++ menu.js
@@ -1,4 +1,14 @@
 $(document).ready(main)
+var contador = 1;
 function main{
-	
-}
+	$('.menu_bar').click(function(){
+		if (contador == 1) {
+			$('nav').animate({
+				left:'0'
+			});
+			contador = 0;
+		}else{contador= 1;$('nav').animate({
+				left:'-100%'
+			});}
+	});
+};
